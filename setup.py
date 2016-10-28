f597bc459643068f5d34b793a0616f2a678afc37
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2016-10-27 23:25:39 -0400

    Setuptools script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pymulator',
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'progress',
+    ],
+    author="Giovanni Luca Ciampaglia",
+    author_email="glciampagl@gmail.com",
+    description="Reproducible simulation for lazy Python programmers",
+    license="GPLv3",
+    keywords="simulation",
+    url="http://github.com/glciampaglia/pymulator"
+)
